d68fa33b29851ecb463ea4a5470081c184512e08
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-07-31 05:39:41 +0100

    added a get locale function

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Basic babel setup"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Config class
+    """
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babe = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get locale function
+    """
+    return request.accept_languages.best_match(app.config)
+
+
+@app.route('/')
+def index():
+    """Default root
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
